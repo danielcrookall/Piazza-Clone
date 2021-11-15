3b4ddb483d158c9938dd92b92d9f2e0ae9daac06
yunishimura0716 <yunishimura0716@gmail.com>
2021-11-14 21:58:11 -0800

    added group by having query

--- index.js
+++ index.js
@@ -195,6 +195,7 @@ app.post('/select', async (req, res, next) => {
 // output render 'problem', keys: problems, courses
 app.get('/problem', (req, res, next) => {
   const context = {};
+  context.averageDiffDept = null;
   connection.query('select * from Problem', (err, result) => {
     if (err) return next(err);
 
@@ -311,6 +312,37 @@ app.post('/problem/select', (req, res, next) => {
 
         context.types = result3;
         res.render('problem/problem', setUserToData(context));
+      });
+    });
+  });
+});
+app.post('/problem/avgDiffDept', (req, res, next) => {
+  const context = {};
+  const operator = (req.body.diffOpr === 'more')? '>' : '<';
+  const queryString = `
+  select department, AVG(difficulty) as avgDiff
+  from Problem
+  group by department
+  having AVG(difficulty) ${operator} ${req.body.diffOpd}
+  `;
+  connection.query(queryString, (err, result) => {
+    if (err) return next(err);
+
+    context.averageDiffDept = result;
+    connection.query('select * from Problem', (err1, result1) => {
+      if (err1) return next(err1);
+  
+      context.problems = result1;
+      connection.query('select * from Course', (err2, result2) => {
+        if (err2) return next(err2);
+  
+        context.courses = result2;
+        connection.query('select * from Type', (err3, result3) => {
+          if (err3) return next(err3);
+  
+          context.types = result3;
+          res.render('problem/problem', setUserToData(context));
+        });
       });
     });
   });
